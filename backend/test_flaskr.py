efc7f1e0cfb7b6276a3ab75078d56539498252ca
mattzshum <mshum002@ucr.edu>
2020-12-28 20:20:02 -0800

    added posts APIs test cases

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -247,6 +247,78 @@ class TopicTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
+    class PostTestCase(unittest.TestCase):
+        def setup(self):
+            self.app = create_app()
+            self.client = self.app.test_client
+            self.database_path = database_path
+            setup_db(self.app, self.database_path)
+
+            with self.app.app_context():
+                self.db = SQLAlchemy()
+                self.db.init_app(self.app)
+                #create all tables
+                self.db.create_all()
+
+            self.sample_post = {
+                'title':'GTFO R4A3',
+                'body':'The only extreme overload we need yet we cannot do it :(',
+                'num_fu':4000,
+                'tag':[],
+                'user_id':1
+            }
+        def test_insert_post(self):
+            res = self.client().post('/posts',
+                                     data=json.dumps(self.sample_post),
+                                     content_type='application/json')
+            data = json.loads(res.data)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIsNotNone(data['created'])
+            self.assertEqual(data['success'], True)
+        
+        def test_insert_post_ERROR(self):
+            res = self.client().post('/posts',
+                                     data=json.dumps({}),
+                                     content_type='application/json')
+            data = json.loads(res.data)
+
+            self.assertEqual(res.status_code, 404)
+            self.assertEqual(data['success'], False)
+            
+        def test_get_posts(self):
+            res = self.client().get('/posts')
+            data = json.loads(res.data)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(data['success'], True)
+            self.assertIsNotNone(data['data'])
+            self.assertIsNotNone(data['total_posts'])
+        
+        #TODO: No test cases because we cannot pagify yet
+        # def test_get_posts_ERROR(self):
+
+        def test_specific_post(self):
+            res = self.client().get('/posts/1')
+            data = json.loads(res.data)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(data['success'], True)
+            self.assertIsNotNone(data['data'])
+        
+        def test_specific_post_ERROR(self):
+            res = self.client().get('/posts/5451')
+            data = json.loads(res.data)
+
+            self.assertEqual(res.status_code, 404)
+            self.assertEqual(data['success'], False)
+
+        def test_delete_post(self):
+            res = self.client().delete('/posts/1')
+            data = json.loads(res.data)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(data['deleted'], 1)            
     
 if __name__ == '__main__':
     unittest.main()
